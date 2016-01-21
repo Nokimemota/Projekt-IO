b6d96132c4a30eb8efc6f74d1c8787027b8c229a
Nokimemota <otaku0kun@gmail.com>
2016-01-22 00:13:19 +0100

    Bugfix
    poprawiony drobny błąd

--- Wyznaczasz.cpp
+++ Wyznaczasz.cpp
@@ -2,80 +2,68 @@
 std::vector<int> Wyznaczasz::Znajdz_trase(std::vector<sf::Vector2f> & punkty,std::vector<droga> & drogi, int punkt_A,int punkt_B)
 {
 	std::vector<int> trasa;
-	int D[25];
-	int i, j, k, N;
-
-	//wyznaczanie najkrotszej drogi z danego punktu do wszystkich innych
-
-	for(i=0;i<(int)punkty.size();i++) D[i]=A[0][i];
-
-	for(k=punkt_A;k<(int)punkty.size()-1;k++)
+	struct djikstra
 	{
-		for(i=0;i<(int)punkty.size();i++)
+		int poprzednik;
+		int droga_poprzednika;
+		float koszty_dojscia;
+		bool ustawione;
+		bool przeszukal;
+		std::vector<int> sasiedztwa;
+		std::vector<float> odleglosci;
+		std::vector<int> droga;
+	};
+
+	std::vector<djikstra> algo(punkty.size());
+
+	for(auto & elem:algo)
+	{
+		elem.ustawione = 0;
+		elem.przeszukal = 0;
+	}
+	algo[punkt_A].ustawione = 1;
+	algo[punkt_A].koszty_dojscia = 0;
+
+	//std::vector<bool> wszerz(punkty.size());
+	for(int i = 0 ;i<drogi.size();i++)
+	{
+		float odleglosc = sqrt(pow(punkty[drogi[i].a].x -punkty[drogi[i].b].x,2 ) +pow(punkty[drogi[i].a].y -punkty[drogi[i].b].y,2 ));
+		algo[drogi[i].a].sasiedztwa.push_back(drogi[i].b);
+		algo[drogi[i].a].odleglosci.push_back(odleglosc);
+		algo[drogi[i].a].droga.push_back(i);
+		algo[drogi[i].b].sasiedztwa.push_back(drogi[i].a);
+		algo[drogi[i].b].odleglosci.push_back(odleglosc);
+		algo[drogi[i].b].droga.push_back(i);
+	}
+	
+	bool koniec = 0;
+	while(koniec ==0)
+	{
+		koniec = 1;
+	for(unsigned int i = 0;i<punkty.size();i++)
+		if(algo[i].ustawione)
 		{
-			for(j=0;j<(int)punkty.size();j++)
+			for(unsigned int j = 0;algo[i].przeszukal == 0&&j<algo[i].sasiedztwa.size();j++)
 			{
-				if(D[j]+A[j][i]>nieskonczonosc)
-					N=nieskonczonosc;
-				else
-					N=D[j]+A[j][i];
-				D[i]=min(D[i],N);
+				int sprawdzany = algo[i].sasiedztwa[j];
+				if(algo[sprawdzany].ustawione ==0 || algo[sprawdzany].koszty_dojscia > algo[i].koszty_dojscia + algo[i].odleglosci[j])
+				{
+					algo[sprawdzany].ustawione =1;
+					algo[sprawdzany].koszty_dojscia = algo[i].koszty_dojscia + algo[i].odleglosci[j];
+					algo[sprawdzany].poprzednik = i;
+					algo[sprawdzany].droga_poprzednika = j;
+					algo[sprawdzany].przeszukal =0;
+					koniec = 0;
+
+				}
 			}
+			algo[i].przeszukal =1;
+
 		}
 	}
+	for(int i = punkt_B; i != punkt_A;i = algo[i].poprzednik)
+		trasa.push_back(algo[algo[i].poprzednik].droga[algo[i].droga_poprzednika]);
 
-	//wyznaczanie trasy
 
-	int u, v;
-	std::vector<int> poprzedniki;
-
-	trasa.push_back(punkt_B);
-	v=punkt_B;
-
-	while(v!=punkt_A)
-	{
-		poprzedniki=Poprzedniki(punkty,drogi,v);
-
-		for(i=0;i<poprzedniki.size();i++)
-		{
-			if(D[v]==D[poprzedniki[i]]+A[poprzedniki[i]][v])
-				u=poprzedniki[i];
-		}
-		trasa.push_back(u);
-		v=u;
-	}
-
-	///tu trzeba zrobic algorytm znajdujacy polaczenie od a do b
-	//trasa.push_back(0);//przyklad
-	//trasa.push_back(1);//przyklad
-	//trasa.push_back(2);//przyklad
 	return trasa;
-}
-
-//wyznaczanie poprzednikow wierzcholka koncowego punkt_B
-
-std::vector<int> Wyznaczasz::Poprzedniki(std::vector<sf::Vector2f> & punkty,std::vector<droga> & drogi, int x)
-{
-	int i, j;
-
-	for(i=0;i<(int)punkty.size();i++)
-	{
-	for(j=0;j<(int)punkty.size();j++)
-	{
-		if(i==j) A[i][j]=nieskonczonosc;
-		else if(drogi[i].dlugosc!=0)
-			A[i][j]=drogi[i].dlugosc;
-		else
-			A[i][j]=nieskonczonosc;
-	}
-	}
-
-	std::vector<int> wynik;
-
-	for(i=0;i<(int)punkty.size();i++)
-	{
-		if((A[i][x]!=nieskonczonosc)&&(A[i][x]!=0))
-			wynik.push_back(i);
-	}
-	return wynik;
 }
--- Wyznaczasz.h
+++ Wyznaczasz.h
@@ -3,17 +3,13 @@
 #include<iostream>
 #include<string>
 #include <vector>
+#include <math.h>
 #include "SFML\Window.hpp"
 #include "punkty.h"
 
-#define min(a,b) (a,b)?a:b
-#define nieskonczonosc 200
-
 class Wyznaczasz
 {
-	int A[25][25];
 public:
 	std::vector<int> Znajdz_trase(std::vector<sf::Vector2f> & punkty,std::vector<droga> & drogi, int punkt_A,int punkt_B);
-	std::vector<int> Poprzedniki(std::vector<sf::Vector2f> & punkty,std::vector<droga> & drogi, int x);
 };
 #endif
